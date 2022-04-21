491eb12ff13716e1371f183f728753fe93f1a77f
pauloLuter <98210796+pauloLuter@users.noreply.github.com>
2022-04-21 13:51:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import pymorphy2
 from datetime import datetime
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
@@ -11,27 +12,28 @@ logger = logging.getLogger(__name__)
 
 TOKEN = '5324119937:AAE2Oheh3gw7v-Brh64iFuWGkhPg4o6xPiU'
 
+d = {'информатика': '20.06.2022', 'биология': '14.06.2022', 'география': '26.05.2022', 'литература': '26.05.2022',
+     'химия': '26.05.2022', 'русский': '30.05.2022', 'математика': '03.06.2022', 'история': '06.06.2022', 'физика': '06.06.2022',
+     'обществознание': '09.06.2022'}
+morph = pymorphy2.MorphAnalyzer()
+
 
 def echo(update, context):
     update.message.reply_text(write(update, context))
 
 
 def read(update, context):
-    if 'биология' in context:
-        a = datetime.today().date().strftime("%d/%m/%Y")
-        update.message.reply_text(f'до ЕГЭ по биологии осталось {a} дней')
-    # if 'химия' in context:
-    #     update.message.reply_text('до ЕГЭ по химии осталось 40 дней')
-    # if 'математика' in context:
-    #     update.message.reply_text('до ЕГЭ по осталось 40 дней')
-    # if 'русский' in context:
-    #     update.message.reply_text('до ЕГЭ по биологии осталось 40 дней')
-    # if 'литература' in context:
-    #     update.message.reply_text('до ЕГЭ по биологии осталось 40 дней')
-    # if 'физика' in context:
-    #     update.message.reply_text('до ЕГЭ по биологии осталось 40 дней')
-    # if 'информатика' in context:
-    #     update.message.reply_text('до ЕГЭ по биологии осталось 40 дней')
+    context = context.lower()
+    context = morph.parse(context)[0]
+    for i in d.keys():
+        if i in context.inflect({'nomn'}).word:
+            a = datetime.today().date().strftime("%d.%m.%Y")
+            a = datetime.strptime(a, "%d.%m.%Y")
+            b = datetime.strptime(d[i], "%d.%m.%Y")
+            i = morph.parse(i)[0]
+            sub = i.inflect({'datv'}).word
+            day = morph.parse('день')[0]
+            update.message.reply_text(f'до ЕГЭ по {sub} осталось {(b - a).days} {day.make_agree_with_number((b - a).days).word}')
 
 
 def write(update, context):
