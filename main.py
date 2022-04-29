e1a920ecbe7ef6e7f2a8a3112c3d5d64564d22c9
pauloLuter <98210796+pauloLuter@users.noreply.github.com>
2022-04-29 15:35:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import logging
 import pymorphy2
 from datetime import datetime
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 # Запускаем логгирование
 logging.basicConfig(
@@ -19,14 +20,28 @@ morph = pymorphy2.MorphAnalyzer()
 
 
 def echo(update, context):
-    update.message.reply_text(write(update, context))
+    update.message.reply_text(start(update, context))
+
+
+def start(update, context):
+    keyboard = [
+        [InlineKeyboardButton("Хочу мотивационную картинку", callback_data='1')],
+        [InlineKeyboardButton("Посоветуй сайты для подготовки", callback_data='2')],
+        [InlineKeyboardButton("Сколько дней осталось до моего экзамена", callback_data='3')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Выберите один из вариантов:', reply_markup=reply_markup)
+
+    write(update, context)
 
 
 def read(update, context):
     context = context.lower()
     context = morph.parse(context)[0]
+    k = 0
     for i in d.keys():
         if i in context.inflect({'nomn'}).word:
+            k = 1
             a = datetime.today().date().strftime("%d.%m.%Y")
             a = datetime.strptime(a, "%d.%m.%Y")
             b = datetime.strptime(d[i], "%d.%m.%Y")
@@ -34,18 +49,25 @@ def read(update, context):
             sub = i.inflect({'datv'}).word
             day = morph.parse('день')[0]
             update.message.reply_text(f'до ЕГЭ по {sub} осталось {(b - a).days} {day.make_agree_with_number((b - a).days).word}')
+    if k == 0:
+        update.message.reply_text('Пока что я не умею понимать все сообщения :(')
 
 
 def write(update, context):
     context = update.message.text
     read(update, context)
 
-#
-#
-# def wrong(update, context):
-#     update.message.reply_text('нет, не так')
-#     update.message.reply_text(a[k + 1])
-#     write(update, context, k)
+
+def button(update, context):
+    query = update.callback_query
+    variant = query.data
+    query.answer()
+    if variant == '1':
+        query.edit_message_text(text=f"Выбранный вариант: {variant}")
+    elif variant == '2':
+        query.edit_message_text(text=f"На этом сайте много материалов для подготовки: {'https://ege.sdamgia.ru/'}")
+    else:
+        query.edit_message_text(text="Какой экзамен ты будешь сдавать?")
 
 
 def main():
@@ -54,8 +76,9 @@ def main():
     text_handler = MessageHandler(Filters.text, echo)
     dp.add_handler(text_handler)
     dp.add_handler(CommandHandler('read', read))
-    # dp.add_handler(CommandHandler('write', write))
-    # dp.add_handler(CommandHandler('wrong', wrong))
+    dp.add_handler(CommandHandler('write', write))
+    dp.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.start_polling()
     updater.idle()
 
